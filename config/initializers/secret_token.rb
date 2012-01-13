697131f70e880727b9586aa303ff30372dd7a3b8
Gideon jadlovker <gideonj1@gmail.com>
2012-01-13 12:52:28 -0500

    new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjectsGravy::Application.config.secret_token = '3a5cb21df50f3e81d6e8200b00f8e7413e22e31eed75ba2a5eab4b6c1dee7830bf53a4a41466c4d751fc0e7a86397f429331378df571262ecd0821af81201b04'
