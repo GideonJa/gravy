d5eaa130885fcc92d6ef3aef96261d85c5071549
Gideon jadlovker <gideonj1@gmail.com>
2012-02-10 17:32:49 -0500

    added email to user, changed first page table, configured Rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,15 @@ class User
    
   index [[:uid, Mongo::ASCENDING], [:provider, Mongo::ASCENDING]]
   
-   
+  validates_presence_of :provider, :uid, :email, :name, :email
+  validates_uniqueness_of :email
   
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["info"]["name"]
+      user.email = auth["info"]["email"]
     end
   end
         
